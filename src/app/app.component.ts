1936c88ca60ba5c96e3aa61c810440d9627253e7
Rafa Garrido <rgarrido@stratio.com>
2016-05-18 16:16:22 +0200

    Add ng2-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { TranslateService, TranslatePipe } from 'ng2-translate/ng2-translate';
 
 @Component({
   selector: 'app',
   template: require('./app.component.html'),
   styles: [require('./app.component.scss')],
+  pipes: [TranslatePipe],
   encapsulation: ViewEncapsulation.None
 })
 
-export class AppComponent { }
+export class AppComponent {
+  constructor(translate: TranslateService) {
+    let userLang = navigator.language.split('-')[0];
+    userLang = /(es|en)/gi.test(userLang) ? userLang : 'en';
+
+    translate.setDefaultLang('en');
+    translate.use(userLang);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
+import { Component, Injectable, provide, enableProdMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { HTTP_PROVIDERS, Http } from '@angular/http';
+import { TranslateService, TranslateLoader, TranslateStaticLoader} from 'ng2-translate/ng2-translate';
 
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
 
-bootstrap(AppComponent, []);
+bootstrap(AppComponent, [
+  HTTP_PROVIDERS,
+  provide(TranslateLoader, {
+    useFactory: (http: Http) => new TranslateStaticLoader(http, '/src/assets/langs', '.json'),
+    deps: [Http]
+  }),
+  TranslateService
+]);
