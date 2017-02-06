e6b842c2e294fb0df9d3ad909995d87e5c46a647
jcnunez-st <jcnunez@stratio.com>
2017-02-06 16:11:16 +0100

    shared module hinting

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from "ng2-translate";
+import { TranslateModule } from 'ng2-translate';
 
 @NgModule({
    imports: [
