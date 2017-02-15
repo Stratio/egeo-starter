669ff7021e9d0f8ce1ac8eb775d5db6c5c368ca5
gjimenez-st <gjimenez@stratio.com>
2017-02-15 18:51:45 +0100

    #118 fix webpack karma config

--- config/webpack/webpack.test.js
+++ config/webpack/webpack.test.js
@@ -1,67 +1,111 @@
-const common = require('./webpack.common');
 const helpers = require('../helpers');
-const path = require('path');
-const webpackMerge = require('webpack-merge');
-
-const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
+const ProvidePlugin = require('webpack/lib/ProvidePlugin');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
 const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
+const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
 
-var config = {
+const ENV = process.env.ENV = process.env.NODE_ENV = 'test';
+
+module.exports = {
    devtool: 'inline-source-map',
+   resolve: {
+      extensions: ['.ts', '.js'],
+      modules: [helpers.root('src'), 'node_modules']
+   },
    module: {
       rules: [
          {
             enforce: 'pre',
             test: /\.js$/,
-            use: 'source-map-loader',
+            loader: 'source-map-loader',
             exclude: [
+               // these packages have problems with their sourcemaps
                helpers.root('node_modules/rxjs'),
                helpers.root('node_modules/@angular')
             ]
          },
          {
             test: /\.ts$/,
-            use: 'awesome-typescript-loader',
-            query: {
-               sourceMap: false,
-               inlineSourceMap: true,
-               compilerOptions: {
-                  removeComments: true
-               }
-            },
+            use: [
+               {
+                  loader: 'awesome-typescript-loader',
+                  query: {
+                     // use inline sourcemaps for "karma-remap-coverage" reporter
+                     sourceMap: false,
+                     inlineSourceMap: true,
+                     compilerOptions: {
+
+                        // Remove TypeScript helpers to be injected
+                        // below by DefinePlugin
+                        removeComments: true
+
+                     }
+                  },
+               },
+               'angular2-template-loader'
+            ],
             exclude: [/\.e2e\.ts$/]
          },
          {
             test: /\.json$/,
-            use: 'json-loader',
+            loader: 'json-loader',
             exclude: [helpers.root('src/index.html')]
          },
          {
             test: /\.css$/,
-            use: ['to-string-loader', 'css-loader'],
-            exclude: [helpers.root('src/index.html')]
-         },
-         {
-            test: /\.html$/,
-            use: 'raw-loader',
+            loader: ['to-string-loader', 'css-loader'],
             exclude: [helpers.root('src/index.html')]
          },
          {
             test: /\.scss$/,
             exclude: '/node_modules/',
-            use: ['raw-loader', 'sass-loader', 'sass-loader?sourceMap']
+            loaders: ['raw-loader', 'sass-loader']
+         },
+         {
+            test: /\.html$/,
+            loader: 'raw-loader',
+            exclude: [helpers.root('src/index.html')]
          },
          {
             enforce: 'post',
             test: /\.(js|ts)$/,
-            use: 'istanbul-instrumenter-loader',
+            loader: 'istanbul-instrumenter-loader',
             include: helpers.root('src'),
             exclude: [
                /\.(e2e|spec)\.ts$/,
                /node_modules/
             ]
          }
+
       ]
+   },
+   plugins: [
+      new DefinePlugin({
+         'ENV': JSON.stringify(ENV),
+         'HMR': false,
+         'process.env': {
+            'ENV': JSON.stringify(ENV),
+            'NODE_ENV': JSON.stringify(ENV),
+            'HMR': false,
+         }
+      }),
+      new ContextReplacementPlugin(
+            // The (\\|\/) piece accounts for path separators in *nix and Windows
+            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+            helpers.root('src'), // location of your src
+            {
+               // your Angular Async Route paths relative to this root directory
+            }
+      ),
+      new LoaderOptionsPlugin({
+         debug: false,
+         options: {
+            // legacy options go here
+         }
+      })
+   ],
+   performance: {
+      hints: false
    },
    node: {
       global: true,
@@ -70,24 +114,5 @@ var config = {
       module: false,
       clearImmediate: false,
       setImmediate: false
-   },
-   plugins: [
-      new ContextReplacementPlugin(
-         /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
-         helpers.root('src'),
-         {
-         }
-      ),
-      new LoaderOptionsPlugin({
-         debug: true,
-         options: {
-         }
-      })
-   ],
-   resolve: {
-      extensions: ['.ts', '.js'],
-      modules: [path.resolve(__dirname, 'src'), 'node_modules']
    }
 };
-
-module.exports = webpackMerge(common, config);
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,15 @@
-/*import {
-  it,
-  inject,
-  describe,
-  beforeEachProviders,
-  expect
-} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
-import { provide } from '@angular/core';
-import { HTTP_PROVIDERS, Http } from '@angular/http';
-import { TranslateService, TranslateLoader, TranslateStaticLoader} from 'ng2-translate/ng2-translate';
 
 describe('App', () => {
-  beforeEachProviders(() => [
-    AppComponent,
-    HTTP_PROVIDERS,
-    provide(TranslateLoader, {
-      useFactory: (http: Http): any => new TranslateStaticLoader(http, '/src/assets/langs', '.json'),
-      deps: [Http]
-    }),
-    TranslateService
-  ]);
+   // provide our implementations or mocks to the dependency injector
+   beforeEach(() => TestBed.configureTestingModule({
+         declarations: [AppComponent]
+      })
+   );
 
-  it ('should work', () => {
-    // Add real test here
-    expect(2).toBe(2);
-  });
+   it('should be true', () => {
+      expect(true).toEqual(true);
+   });
 });
-*/
