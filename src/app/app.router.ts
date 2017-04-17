affb3eefec3aab2e4a3fea0aba725eef44347ebf
Jose Núñez <jcnunez@MacBook-Pro-de-Jose.local>
2017-04-17 11:58:22 +0200

    enable errors module

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,6 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MenuModule, MENU_ROUTES } from './menu';
+import { Error404Component } from '@app/errors';
+
+const appRoutes:Routes = [
+   ...MENU_ROUTES,
+   { path: 'messages', loadChildren: './messages/index#MessagesModule' },
+   { path: 'settings', loadChildren: './settings/index#SettingsModule' },
+   {
+      path: '**',
+      component: Error404Component
+   }
+];
 
 @NgModule({
    exports: [
@@ -10,14 +21,7 @@ import { MenuModule, MENU_ROUTES } from './menu';
    ],
    imports: [
       MenuModule,
-      RouterModule.forRoot(
-         [
-            ...MENU_ROUTES,
-            { path: 'messages', loadChildren: './messages/index#MessagesModule' },
-            { path: 'settings', loadChildren: './settings/index#SettingsModule' }
-         ],
-         { enableTracing: true }
-      )
+      RouterModule.forRoot(appRoutes, { enableTracing: true })
    ]
 })
 
